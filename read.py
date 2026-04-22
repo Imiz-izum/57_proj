87807f2f1ca4904e5a240606ce940100d43151fc
Никита Ачилов <nikitushka.achilov@gmail.com>
2026-04-22 21:39:04 +0300

    opt_up

--- read.py
+++ read.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 import serial
 
-port = serial.Serial('COM5', 9600, timeout=1)
+port = serial.Serial('COM4', 9600, timeout=1)
 
 
 
@@ -15,43 +15,40 @@ k = float(input())
 m = float(input())
 f_time = float(input()) # время на которое мы моделируем 
 
+plt.ion()
+fig, ax = plt.subplots()
+
 while True:
-
-    isfloat = False
-    while isfloat != True:
-            try:
-                data = float(port.readline().decode('utf-8').strip())
-                isfloat = True
-            except ValueError:
-                 pass
-
+    try:
+        data = float(port.readline().decode('utf-8').strip())
+        print(data)  # :point_left: проверь, что значения реально меняются
+    except:
+        continue
 
     f_x = data * f_time
 
-    ang = math.acos(  
-    (k * f_x / m) /
-    math.sqrt(
-        (k * f_x / m)**2 +
-        (g * (f_time - m / k * (1 - math.exp(-k * f_time / m))))**2
+    ang = math.acos(
+        (k * f_x / m) /
+        math.sqrt(
+            (k * f_x / m)**2 +
+            (g * (f_time - m / k * (1 - math.exp(-k * f_time / m))))**2
         )
-    ) # подсчет угла в радианах
+    )
 
     v_start = math.sqrt(
-    (k * f_x / (m * (1 - math.exp(-k * f_time / m))))**2 +
-    (g * (f_time / (1 - math.exp(-k * f_time / m)) - m / k))**2
-    ) # подсчет модуля начальной скорости
+        (k * f_x / (m * (1 - math.exp(-k * f_time / m))))**2 +
+        (g * (f_time / (1 - math.exp(-k * f_time / m)) - m / k))**2
+    )
 
-
-
-    t = np.linspace(0, f_time, 30000)
+    t = np.linspace(0, f_time, 500)
 
     xt = v_start*math.cos(ang) * m/k *(1 - np.exp(-k*t/m))
     yt = (v_start*math.sin(ang)*m/k+m*m*g/k**2)*(1-np.exp(-k*t/m)) - m*g*t/k
 
-    plt.ion()
-    
-    plt.plot(xt, yt) 
-    plt.show()
-    plt.pause(2)
+    ax.clear()
+    ax.plot(xt, yt)
 
-    plt.clf()
+    fig.canvas.draw()       # :point_left: принудительная перерисовка
+    fig.canvas.flush_events()
+
+    plt.pause(0.01)
